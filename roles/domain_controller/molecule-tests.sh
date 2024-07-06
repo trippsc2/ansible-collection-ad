4f1e47f133830338079be9478540eb5a2680905b
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2024-07-06 13:53:28 -0400

    Various bug fixes. Added validation where missing. Added more verification to tests.

--- roles/domain_controller/molecule-tests.sh
+++ roles/domain_controller/molecule-tests.sh
@@ -3,6 +3,4 @@
 set -e
 
 MOLECULE_BOX="w2022_cis" molecule test
-MOLECULE_BOX="w2022_cis" molecule test -s new
 MOLECULE_BOX="w2019_cis" molecule test
-MOLECULE_BOX="w2019_cis" molecule test -s new
