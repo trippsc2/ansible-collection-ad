ea542a7ea1d574b9fbdf5ce14556e80a17cc9985
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2025-03-05 08:32:42 -0500

    More test cleanup.

--- roles/domain_controller/molecule-tests.sh
+++ roles/domain_controller/molecule-tests.sh
@@ -4,7 +4,9 @@ set -e
 
 MOLECULE_BOX="w2022_cis" molecule test
 MOLECULE_BOX="w2022_cis" molecule test -s new
+
 MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w2019_cis" molecule test -s new
+
 MOLECULE_BOX="w2025_base" molecule test
 MOLECULE_BOX="w2025_base" molecule test -s new
